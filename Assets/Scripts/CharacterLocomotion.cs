4a8db978c789ae678c2db231c5a994dd7f256223
Thuc Tran <thuc.ttk@gmail.com>
2022-09-29 21:20:03 +0700

    [add] move character with character controller and root motion

--- Assets/Scripts/CharacterLocomotion.cs
+++ Assets/Scripts/CharacterLocomotion.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class CharacterLocomotion : MonoBehaviour
 {
+    public float jumpHeight;
+    public float gravity;
+    public float stepDown;
+    public float airControl;
+    public float jumpDamp;
+    public float groundSpeed;
+    public float pushPower;
+
+    private CharacterController characterController;
     private Animator playerAnimator;
     private Vector2 playerInput;
+    private Vector3 rootMotion;
+    private Vector3 velocity;
+    private bool isJumping;
     
-    // Start is called before the first frame update
     void Start()
     {
         playerAnimator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         playerInput.x = Input.GetAxis("Horizontal");
@@ -21,5 +32,97 @@ public class CharacterLocomotion : MonoBehaviour
 
         playerAnimator.SetFloat("InputX", playerInput.x);
         playerAnimator.SetFloat("InputY", playerInput.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isJumping)
+        {
+            UpdateInAir();
+        }
+        else
+        {
+            UpdateOnGround();
+        }
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+            return;
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3f)
+            return;
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.velocity = pushDir * pushPower;
+    }
+
+    private void UpdateOnGround()
+    {
+        Vector3 stepForward = rootMotion * groundSpeed;
+        Vector3 stepDownAmount = Vector3.down * stepDown;
+
+        characterController.Move(stepForward + stepDownAmount);
+        rootMotion = Vector3.zero;
+
+        if (!characterController.isGrounded)
+        {
+            SetInAir(0);
+        }  
+    }
+
+    private void UpdateInAir()
+    {
+        velocity.y -= gravity * Time.fixedDeltaTime;
+        Vector3 displacement = velocity * Time.fixedDeltaTime;
+        displacement += CalculateAirControl();
+        characterController.Move(displacement);
+        isJumping = !characterController.isGrounded;
+        rootMotion = Vector3.zero;
+        playerAnimator.SetBool("isJumping", isJumping);
+    }
+
+    private void OnAnimatorMove()
+    {
+        rootMotion += playerAnimator.deltaPosition;
+    }
+
+    private void Jump()
+    {
+        if (!isJumping)
+        {
+            float jumpVelocity = Mathf.Sqrt(2 * gravity * jumpHeight);
+            SetInAir(jumpVelocity);
+        }
+    }
+
+    private void SetInAir(float jumpVelocity)
+    {
+        isJumping = true;
+        velocity = playerAnimator.velocity * jumpDamp * groundSpeed;
+        velocity.y = jumpVelocity;
+        playerAnimator.SetBool("isJumping", true);
+    }
+
+    private Vector3 CalculateAirControl()
+    {
+        return ((transform.forward * playerInput.y) + (transform.right * playerInput.x)) * (airControl / 100);
     }
 }
